801886b0952d267240293b7d3d1560e826685c68
Jordi Calafat <104111071+jocades@users.noreply.github.com>
2024-12-17 12:02:22 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,19 @@
-use std::io::{self, BufRead, Write};
+use std::{
+    env, fs,
+    io::{self, BufRead, Write},
+};
 
-use anyhow::Result;
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
 const BUILTINS: [&str; 3] = ["exit", "echo", "type"];
 
 fn main() -> Result<()> {
     let mut stdin = io::stdin().lock();
+    let mut buf = String::new();
+
     loop {
         print!("$ ");
         io::stdout().flush()?;
-        let mut buf = String::new();
         stdin.read_line(&mut buf)?;
 
         let cmd: Vec<&str> = buf.split_whitespace().collect();
@@ -20,10 +24,27 @@ fn main() -> Result<()> {
                 if BUILTINS.iter().any(|&b| b == cmd[1]) {
                     println!("{} is a shell builtin", cmd[1]);
                 } else {
-                    println!("{}: not found", cmd[1]);
+                    let mut ok = false;
+                    for path in env::var("PATH")?.split(':') {
+                        for entry in fs::read_dir(path)?.filter_map(|e| e.ok()) {
+                            if entry.file_name() == cmd[1] {
+                                println!("{} is {}", cmd[1], entry.path().display());
+                                ok = true;
+                                break;
+                            }
+                        }
+                        if ok {
+                            break;
+                        }
+                    }
+                    if !ok {
+                        println!("{}: not found", cmd[1]);
+                    }
                 }
             }
             other => println!("{other}: command not found"),
-        }
+        };
+
+        buf.clear();
     }
 }
