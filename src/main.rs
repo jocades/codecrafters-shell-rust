3406ffaf4ecf733b69450c466819f390b4547ca2
Jordi Calafat <104111071+jocades@users.noreply.github.com>
2024-12-17 10:51:46 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::io::{self, BufRead, Write};
 
-use anyhow::{Context, Result};
+use anyhow::Result;
+
+const BUILTINS: [&str; 3] = ["exit", "echo", "type"];
 
 fn main() -> Result<()> {
     let mut stdin = io::stdin().lock();
@@ -10,10 +12,17 @@ fn main() -> Result<()> {
         let mut buf = String::new();
         stdin.read_line(&mut buf)?;
 
-        let cmd: Vec<&str> = buf.trim().split_whitespace().collect();
+        let cmd: Vec<&str> = buf.split_whitespace().collect();
         match cmd[0] {
             "exit" => std::process::exit(cmd[1].parse()?),
             "echo" => println!("{}", &cmd[1..].join(" ")),
+            "type" => {
+                if BUILTINS.iter().any(|&b| b == cmd[1]) {
+                    println!("{} is a shell builtin", cmd[1]);
+                } else {
+                    println!("{}: not found", cmd[1]);
+                }
+            }
             other => println!("{other}: command not found"),
         }
     }
