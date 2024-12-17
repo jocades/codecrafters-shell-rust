795de30cc87c6e3783d308301dd0adacaf1c8db7
Jordi Calafat <104111071+jocades@users.noreply.github.com>
2024-12-17 10:30:33 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 use std::io::{self, BufRead, Write};
 
-use anyhow::Result;
+use anyhow::{Context, Result};
 
 fn main() -> Result<()> {
     let mut stdin = io::stdin().lock();
-
     loop {
         print!("$ ");
         io::stdout().flush()?;
@@ -12,7 +11,13 @@ fn main() -> Result<()> {
         stdin.read_line(&mut buf)?;
 
         match buf.trim() {
-            cmd => println!("{cmd}: command not found"),
+            cmd if cmd.starts_with("exit") => {
+                let (_, code) = cmd.split_once(" ").context("must include code")?;
+                std::process::exit(code.parse()?)
+            }
+            cmd => {
+                println!("{cmd}: command not found");
+            }
         };
     }
 
