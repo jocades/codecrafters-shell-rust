4a5eadadb36fcc7e1b6c75465277359dc55132ca
Jordi Calafat <104111071+jocades@users.noreply.github.com>
2024-12-17 10:21:57 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,18 @@ use std::io::{self, BufRead, Write};
 use anyhow::Result;
 
 fn main() -> Result<()> {
-    print!("$ ");
-    io::stdout().flush()?;
-
     let mut stdin = io::stdin().lock();
-    let mut buf = String::new();
-    stdin.read_line(&mut buf)?;
 
-    match buf.trim() {
-        cmd => println!("{cmd}: command not found"),
-    };
+    loop {
+        print!("$ ");
+        io::stdout().flush()?;
+        let mut buf = String::new();
+        stdin.read_line(&mut buf)?;
+
+        match buf.trim() {
+            cmd => println!("{cmd}: command not found"),
+        };
+    }
 
     Ok(())
 }
